41f46428266b98e51501820e880c2e95cb89863f
Robin Lake <mister.lake@gmail.com>
2017-06-01 20:18:16 -0600

    Everything works locally; still set to localhost

--- server.js
+++ server.js
@@ -9,9 +9,40 @@ app.get('/', function (req, res) {
 })
 
 app.get('/:time', function (req, res) {
-  res.send('Your input was: ' + req.params.time + dateUnix)
-  var dateUnix = Date.parse(req.params.time)
+  if (isNaN(req.params.time)){
+  var dateUnix = Date.parse(req.params.time.toString())
+  console.log('request' + typeof req.params.time + isNaN(req.params.time));
+  } else {
+    var dateUnix = parseInt(req.params.time);
+    console.log('request' +typeof req.params.time + isNaN(req.params.time));
+  }
+  var dateNatural =  new Date(dateUnix)
+  console.log('dateNatural = ' + dateNatural);
+  var months = new Array(12);
+    months[0] = "January";
+    months[1] = "February";
+    months[2] = "March";
+    months[3] = "April";
+    months[4] = "May";
+    months[5] = "June";
+    months[6] = "July";
+    months[7] = "August";
+    months[8] = "September";
+    months[9] = "October";
+    months[10] = "November";
+    months[11] = "December";
+
+  var dateMonth = months[dateNatural.getMonth()];
+  var dateDay = dateNatural.getDate();
+  var dateYear = dateNatural.getFullYear();
+
+  if( isNaN(dateUnix) || isNaN(dateDay)){
+    res.send('Unix Timestamp: null  Natural Language: null')
+  } else {
+  res.send('Unix Timestamp: ' + dateUnix + ' ' + "Natural Language:" + ' ' + dateMonth + ' ' + dateDay + ', ' + dateYear)
+  }
 })
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
